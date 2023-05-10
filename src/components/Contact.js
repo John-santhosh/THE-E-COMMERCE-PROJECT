b601fcbc5ef6cbe8b004ef9df7f53b199a142909
john <johnsantosh2@gmail.com>
2023-05-10 10:40:52 +0530

    main components

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,35 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const Contact = () => {
-  return <h4>contact section</h4>
-}
+  return (
+    <Wrapper>
+      <div className="section-center">
+        <h3>Join our newletter and get 20% off</h3>
+        <div className="content">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit nam
+            placeat reprehenderit facilis minima unde atque? Ab mollitia
+            sapiente enim.
+          </p>
+          <form
+            className="contact-form"
+            action="https://formspree.io/f/xzbqonao"
+            method="POST"
+          >
+            <input
+              placeholder="powered by FormSpree, enter email"
+              type="email"
+              name="email"
+              className="form-input"
+            />
+            <button className="submit-btn">subscribe</button>
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
 const Wrapper = styled.section`
   padding: 5rem 0;
   h3 {
@@ -67,6 +93,6 @@ const Wrapper = styled.section`
   @media (min-width: 1280px) {
     padding: 15rem 0;
   }
-`
+`;
 
-export default Contact
+export default Contact;
